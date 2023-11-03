bee6875d47479cd9d02c03b42c122125ff821e51
Abigail S <abiskepple@gmail.com>
2023-11-03 01:01:51 -0400

    Edited files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-/* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded',function(){
+	const form = document.getElementById('newsletter-form');
+	const messageDiv = document.querySelector('.message');
+	
+	form.addEventListener('submit',function(event){
+		event.preventDefault();
+		
+		const emailInput = document.getElementById('email');
+		const emailAddress = emailInput.value;
+		
+		if(emailAddress.trim() === ''){
+			messageDiv.textContent = 'Please enter a valid email address';
+		}else{
+			messageDiv.textContent = 'Thank you! Your email address ${emailAddress} has been added to our mailing list!';
+		
+		emailnput.value = '';
+	});
+});
